825a18c8e42e8d3745edf82d98fe41f3d258ba91
Sam <samkian0007@gmail.com>
2026-06-19 13:05:56 +0200

    make first API call

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ import OpenAI from "openai";
 const client = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
+
+const response = await client.responses.create({
+  model: "gpt-4.1-mini",
+  input: "Give three interesting facts about Stockholm. Answer in under 50 words."
+});
+
+console.log(response);
